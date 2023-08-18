1d7eb84b61f8b0eee256720f0ee5c7c7f790e47e
ricardoitv <120180055+ricardoitv@users.noreply.github.com>
2023-08-18 14:27:30 +0100

    Refactored Main to be Scala 2 compatible

--- build.sbt
+++ build.sbt
@@ -3,14 +3,12 @@ import com.typesafe.sbt.SbtNativePackager.{Universal => _, _}
 import NativePackagerHelper._
 import scala.io.Source
 
-val scala3Version = "3.3.0"
-
 lazy val root = project
   .in(file("."))
   .enablePlugins(JavaAppPackaging)
   .settings(
     name := "Cyclic reference involving object",
     version := "0.1.0-SNAPSHOT",
-    scalaVersion := scala3Version,
+    scalaVersion := "3.3.0", // "2.13.11", // "3.3.0"
     libraryDependencies ++= List("software.amazon.awssdk" % "sts" % "2.20.69")
   )
--- src/main/scala/Main.scala
+++ src/main/scala/Main.scala
@@ -2,16 +2,18 @@ import software.amazon.awssdk.auth.credentials.AwsCredentialsProvider
 import software.amazon.awssdk.auth.credentials.WebIdentityTokenFileCredentialsProvider
 import java.nio.file.Paths
 
-@main def hello: Unit =
+object Main extends App {
   println("Hello world!")
   println(msg)
   somethingElse()
 
-def msg = "I was compiled by Scala 3. :)"
+  def msg = "blah"
 
-def somethingElse(): AwsCredentialsProvider =
-  WebIdentityTokenFileCredentialsProvider.builder
-    .roleArn("dummy")
-    .roleSessionName("Dummy")
-    .webIdentityTokenFile(Paths.get("."))
-    .build
+  def somethingElse(): AwsCredentialsProvider = {
+    WebIdentityTokenFileCredentialsProvider.builder
+      .roleArn("dummy")
+      .roleSessionName("Dummy")
+      .webIdentityTokenFile(Paths.get("."))
+      .build
+  }
+}
