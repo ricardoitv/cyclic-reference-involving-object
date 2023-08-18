38e73c1c654cf2a677067f0d1ccf70a380301fa8
ricardoitv <120180055+ricardoitv@users.noreply.github.com>
2023-08-18 14:59:33 +0100

    Simplifying the Main class

--- src/main/scala/Main.scala
+++ src/main/scala/Main.scala
@@ -1,19 +1,10 @@
-import software.amazon.awssdk.auth.credentials.AwsCredentialsProvider
 import software.amazon.awssdk.auth.credentials.WebIdentityTokenFileCredentialsProvider
 import java.nio.file.Paths
 
 object Main extends App {
-  println("Hello world!")
-  println(msg)
-  somethingElse()
-
-  def msg = "blah"
-
-  def somethingElse(): AwsCredentialsProvider = {
-    WebIdentityTokenFileCredentialsProvider.builder
-      .roleArn("dummy")
-      .roleSessionName("Dummy")
-      .webIdentityTokenFile(Paths.get("."))
-      .build
-  }
+  WebIdentityTokenFileCredentialsProvider.builder
+    .roleArn("dummy")
+    .roleSessionName("Dummy")
+    .webIdentityTokenFile(Paths.get("."))
+    .build
 }
